360069a7627ac22131473adb0963de02809e84b5
Simona <see.moona@yahoo.com>
2020-02-04 10:08:57 +0100

    update

--- 03.03.20 objects.js
+++ 03.03.20 objects.js
@@ -18,9 +18,23 @@ let aliAdd = 'xxx str';
 let aliAge = 22;
 let aliLastName = 'something';
 
-const objectc = {
+const object = {
     color:'Red',
     size:'big',
     year:1990,
     arr:[1,3,4,5,6]
 };
+
+
+const car = {};
+console.log(car);
+car['name'] = 'Ford';
+console.log(car);
+car['year'] = 2020;
+console.log(car);
+
+car.age = 22;
+console.log(car);
+car.age = 44;
+car.fa = 'hhh';
+console.log(car);
